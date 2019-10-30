ca5b7172d9ea158a55a82fa560922fb1074b8028
Calvo Almazan <irenealmazan@hotmail.com>
2019-10-30 13:47:37 -0500

    sosvpe_sf_miscut_dt_allgrowthrtes_Cdt prints all the figures which are shown in the powerpoint presentation Figures_XPCS_paper_stepflow_v2

--- sosvpe_sf_miscut_dt_allgrowthrates_Cdt.m
+++ sosvpe_sf_miscut_dt_allgrowthrates_Cdt.m
@@ -23,9 +23,6 @@ color_rates_allgrowth = ['or';'ok';'om';'ob';'og';'oy';'oc';'xb'];%['k'];%
 path_save_fig = ['./figures_presentation_v6'];
 
 
-skip = 1;
-
-
 
 for ss = 1:size(runnames,1)
     
@@ -33,7 +30,7 @@ for ss = 1:size(runnames,1)
     runname = [paths{ss,:} runnames{ss,:}];
     runname_title = runname_titles{ss,:};
     
-   
+    
     load([runname '_corr_dt_L0p5.mat'],'Cdt','ddam');
     
     Cdt_struct(ss).Cdt = Cdt;
@@ -44,10 +41,11 @@ for ss = 1:size(runnames,1)
     
 end
 
+ 
+
 %% fit the CdtI with two single exponential decays (taking as the fast decay time constant, 
 %  the time constant obtained from the fit of the 0 growth rate)
 
-skip_fit = 1;
 
 if ~skip_fit
     
@@ -209,26 +207,6 @@ end
 
 %%
 
-%{
-for ss = 1:size(runnames,1)
-    path = paths{ss,:};
-    damHW_file = [path 'damHW_fit.mat'];%[path 'damHW.mat'];%
-    load(damHW_file);
-    
-    if strcmp(damHW_file,[path 'damHW.mat'])
-        Cdt_struct(ss).damHW = damHW./log(2);
-        Cdt_struct(ss).back = zeros(size(damHW));
-        Cdt_struct(ss).contrast = ones(size(damHW));
-    elseif strcmp(damHW_file,[path 'damHW_fit.mat'])
-        Cdt_struct(ss).damHW = damHW;
-        Cdt_struct(ss).back = back;
-        Cdt_struct(ss).contrast = contrast;
-    end
-    
-end
-
-%}
-
 return;
 
 %% Initialize parameters to plot
@@ -275,7 +253,7 @@ struct_ranges.ixaoff = ixaoff;
 struct_ranges.nsteps = nsteps;
 
 
-POSITION = [67 298 1236 497];%[ 98 457 2410 275];%[1 415 1233 287];%[1 12 473 690];%[51 47 387 692];
+POSITION = [55 212 723 588];%[67 298 1236 497];%[ 98 457 2410 275];%[1 415 1233 287];%[1 12 473 690];%[51 47 387 692];
 PAPERPOSITION = [1 1 4 3];
 PAPERSIZE = [ 8.5000   11.0000];
 
@@ -357,19 +335,19 @@ Function_display_sos.make_figure_2D(title_figure,Cdt_struct,'damHW_slow_sigma',s
 
 
 
-% Figure 200: Contrast
+% Figure 200: Contrast fast
 figNum = 200;
 % time frame to represent:
 %time_frame = 130;
 clear title_figure;
 for ss = 1:size(runname_titles,1)
     title_figure(ss,:) = {[runname_titles{ss,:} ' Contrast fast']};
-   % map2D(ss).Contrast = Cdt_struct(ss).contrast_slow;
+    map2D(ss).Contrast = 10.^(Cdt_struct(ss).contrast_fast);
 end
 
 
-Function_display_sos.make_figure_2D( title_figure,Cdt_struct,'contrast_fast',struct_ranges,iCTR,figNum,...
-    'CLIM',[-.3 .3],...
+Function_display_sos.make_figure_2D( title_figure,map2D,'Contrast',struct_ranges,iCTR,figNum,...
+    'CLIM',[0 1],...
     'POSITION',POSITION,'PAPERPOSITION',PAPERPOSITION,...
     'plot_averages',0);
 
@@ -379,20 +357,25 @@ figNum = 250;
 % time frame to represent:
 %time_frame = 130;
 clear title_figure;
-for ss = 1:size(runname_titles,1)
+
+ss = 1;
+title_figure(ss,:) = {[runname_titles{ss,:} ' Contrast']};
+map2D(ss).Contrast = 10.^(Cdt_struct(ss).contrast_fast);
+for ss = 2:size(runname_titles,1)
     title_figure(ss,:) = {[runname_titles{ss,:} ' Contrast slow']};
-    %map2D(ss).Contrast = Cdt_struct(ss).contrast_slow;
+    map2D(ss).Contrast = 10.^(Cdt_struct(ss).contrast_slow);
 end
 
 
-Function_display_sos.make_figure_2D( title_figure,Cdt_struct,'contrast_slow',struct_ranges,iCTR,figNum,...
+Function_display_sos.make_figure_2D( title_figure,map2D,'Contrast',struct_ranges,iCTR,figNum,...
     'CLIM',[-.3 .3],...
     'POSITION',POSITION,'PAPERPOSITION',PAPERPOSITION,...
     'plot_averages',0);
 
 
-%{
+
 % Figure 2000: Contrast manual
+%{
 figNum = 2000;
 % time frame to represent:
 %time_frame = 130;
@@ -414,8 +397,9 @@ Function_display_sos.make_figure_2D( title_figure,map2D,'Contrast_manual',struct
     'plot_averages',0);
 %}
 
-%{
 % Figure 300: background
+%{
+
 figNum = 300;
 % time frame to represent:
 %time_frame = 130;
@@ -479,8 +463,8 @@ figNum = 2;
 
 Function_display_sos.make_figures_tauML1D(['\tau_s offset in Q_y = ' num2str(iyaoff)],...
     QX,damHW1D_struct,'tauML_1D_Qx',figNum,...
-    'iCTR',iCTR,'nsteps',nsteps,...
-    'XLABEL','Q_x (pixels)','YLIM',[1e5 1e17]);
+    'iCTR',iCTR,'nsteps',nsteps,'POSITION',POSITION,...
+    'XLABEL','Q_x (pixels)','YLIM',[1e6 1e10]);
 
 legend(legend_str);
 
@@ -490,18 +474,20 @@ figNum = 20;
 Function_display_sos.make_figures_tauML1D(['QX is positive,  offset in Qy = ' num2str(iyaoff)],...
     Qx_positive,damHW1D_struct,'tauML_1D_Qx_positive',...
     figNum,'tauMLth',tauML_th_QX_positive,'iCTR',iCTR,'nsteps',nsteps,'ixaoff',ixaoff,...
-    'XLABEL','Q_x (pixels)',...
+    'XLABEL','Q_x (pixels)','POSITION',POSITION,...
     'XSCALE','log','YSCALE',...
-    'log','YLIM',[1e5 1e17]);
+    'log','YLIM',[1e6 1e10]);
 
 legend(legend_str);
 
 figNum = 21;
 
-Function_display_sos.make_figures_tauML1D(['QX is negative,  offset in Qy = ' num2str(iyaoff)],Qx_negative,damHW1D_struct,'tauML_1D_Qx_negative',...
-    figNum,'tauMLth',tauML_th_QX_negative,'iCTR',iCTR,'nsteps',nsteps,'ixaoff',ixaoff,'XLABEL','Q_x (pixels)',...
+Function_display_sos.make_figures_tauML1D(['QX is negative,  offset in Qy = ' num2str(iyaoff)],...
+    Qx_negative,damHW1D_struct,'tauML_1D_Qx_negative',...
+    figNum,'tauMLth',tauML_th_QX_negative,'iCTR',iCTR,'nsteps',nsteps,'ixaoff',ixaoff,...
+    'XLABEL','Q_x (pixels)','POSITION',POSITION,...
     'XSCALE','log','YSCALE',...
-    'log','YLIM',[1e5 1e17]);
+    'log','YLIM',[1e6 1e10]);
 
 legend(legend_str);
 
@@ -538,127 +524,158 @@ end
 figNum = 4;
 
 Function_display_sos.make_figures_tauML1D(['\tau_s offset in Q_x = ' num2str(ixaoff)],...
-    QY,damHW1D_struct,'tauML_1D_Qy',figNum,...
-    'XLABEL','Q_y (pixels)','YLIM',[1e5 1e17]);
+    QY,damHW1D_struct,'tauML_1D_Qy',figNum,'POSITION',POSITION,...
+    'XLABEL','Q_y (pixels)','YLIM',[1e6 1e10]);
 
 legend(legend_str);
 
 
 figNum = 40;
 
-Function_display_sos.make_figures_tauML1D(['QY is positive,  offset in Qx = ' num2str(ixaoff)],Qy_positive,damHW1D_struct,'tauML_1D_Qy_positive',...
+Function_display_sos.make_figures_tauML1D(['QY is positive,  offset in Qx = ' num2str(ixaoff)],...
+    Qy_positive,damHW1D_struct,'tauML_1D_Qy_positive',...
     figNum,'tauMLth',tauML_th_Qy_positive,'iyaoff',iyaoff,'XLABEL','Q_y (pixels)',...
-    'XSCALE','log','YSCALE',...
-    'log','YLIM',[1e5 1e17]);
+    'XSCALE','log','YSCALE','log','POSITION',POSITION,...
+    'YLIM',[1e6 1e10]);
 
 legend(legend_str);
 
 figNum = 41;
 
-Function_display_sos.make_figures_tauML1D(['QY is negative,  offset in Qx = ' num2str(ixaoff)],Qy_negative,damHW1D_struct,'tauML_1D_Qy_negative',...
+Function_display_sos.make_figures_tauML1D(['QY is negative,  offset in Qx = ' num2str(ixaoff)],...
+    Qy_negative,damHW1D_struct,'tauML_1D_Qy_negative',...
     figNum,'tauMLth',tauML_th_Qy_negative,'iyaoff',iyaoff,'XLABEL','Q_y (pixels)',...
-    'XSCALE','log','YSCALE',...
-    'log','YLIM',[1e5 1e17]);
+    'XSCALE','log','YSCALE','log','POSITION',POSITION,...
+    'YLIM',[1e6 1e10]);
 legend(legend_str);
+
+
+%% Single Q values:
+
+QY_val = 30;
+figure(5);
+clf;
+hold on;
+for ss = 1:size(runnames,1)
+    tauML_1D = mean(Cdt_struct(ss).damHW_fast(:,range_cols),2);
+    damHW1D_struct(ss).tauML_1D_oneQ = tauML_1D(QY_val);
+    
+    plot(growth_rates_all(ss),damHW1D_struct(ss).tauML_1D_oneQ,color_rates_allgrowth(ss,:),'LineWidth',3.0,'MarkerSize',20);
+    
+end
+xlabel('Growth rates (ML/sim units)');
+ylabel('\tau (sim units)');
+set(gca,'FontSize',30);
+box on;
+title(['Time constant vs growth rate at QY = ' num2str(QY_val)]);
+set(gcf,'Position',[534    64   769   538]);
+set(gca,'YScale','log');
+ylim([4e6 3e7]);
+
+QX_val = 30;
+figure(6);
+clf;
+hold on;
+for ss = 1:size(runnames,1)
+    tauML_1D = mean(Cdt_struct(ss).damHW_fast(range_rows,:),1);
+    damHW1D_struct(ss).tauML_1D_oneQ = tauML_1D(QX_val);
+    
+    plot(growth_rates_all(ss),damHW1D_struct(ss).tauML_1D_oneQ,color_rates_allgrowth(ss,:),'LineWidth',3.0,'MarkerSize',20);
+    
+end
+xlabel('Growth rates (ML/sim units)');
+ylabel('\tau (sim units)');
+set(gca,'FontSize',30);
+box on;
+title(['Time constant vs growth rate at QX = ' num2str(QY_val)]);
+set(gcf,'Position',[534    64   769   538]);
+set(gca,'YScale','log');
+ylim([2e7 2e9]);
 
 return;
 
 
-%% Plot ihm
 
 %% load ihm
 
+computer_script;
 
-for ss = 6:size(runnames,1)
+if ihm_flag == 0
     
-     path = paths{ss,:};
-     runname = [paths{ss,:} runnames{ss,:}];
-     runname_title = runname_titles{ss,:};
- 
-    
-     
-     
-%      if strcmp(runname_title ,[ 'miscut\_32\_26'])
-%          load([runname '_ihm_last2000.mat']);
-%          ihm = ihm_resize;
-%          clear ihm_resize;
-%          dtime_orig = load([runname '_stats.mat'],'dtime');
-%          dtime = dtime_orig.dtime(end-size(ihm,3):end);
-%          clear dtime_orig
-%      elseif strcmp(runname_title ,[ 'miscut\_32\_32'])
-%          load([runname '_ihm.mat']);
-%          dtime_orig = load([runname '_stats.mat'],'dtime');
-%          dtime = dtime_orig.dtime;
-%          clear dtime_orig
-%      elseif strcmp(runname_title ,[ 'miscut\_32\_33'])
-%          load([runname '_ihm.mat']);
-%          dtime_orig = load([runname '_stats.mat'],'dtime');
-%          dtime = dtime_orig.dtime;
-%          clear dtime_orig
-
-    if strcmp(runname_title ,[ 'miscut\_32\_29'])
-        load([runname '_ihm.mat']);
-        load([runname '_stats.mat'],'dtime');
-    elseif strcmp(runname_title ,[ 'miscut\_32\_30'])
-        load([runname '_ihm.mat']);
-        load([runname '_stats.mat'],'dtime');
-    elseif strcmp(runname_title ,[ 'miscut\_32\_35'])
-        load([runname '_ihm.mat']);
-        load([runname '_stats.mat'],'dtime');
-    elseif strcmp(runname_title ,[ 'miscut\_32\_36'])
-        load([runname '_ihm.mat']);
-        load([runname '_stats.mat'],'dtime');
-
-    elseif strcmp(runname_title ,[ 'miscut\_32\_37'])
-        load([runname '_ihm_half.mat']);
-        ihm = ihm_half;
-        dtime_orig = load([runname '_stats.mat'],'dtime');
-        dtime = dtime_orig.dtime(end-round(size(ihm_half,3)/2):end);
-        clear ihm_half
-        clear dtime_orig
-    elseif strcmp(runname_title ,[ 'miscut\_32\_38'])
-        load([runname '_ihm_half.mat']);
-        ihm = ihm_half;
-        dtime_orig = load([runname '_stats.mat'],'dtime');
-        dtime = dtime_orig.dtime(end-size(ihm_half,3):end);
-        clear ihm_half
-        clear dtime_orig
-
-    elseif strcmp(runname_title ,[ 'miscut\_32\_39'])
-        load([runname '_ihm_half.mat']);
-        ihm = ihm_half;
-        dtime_orig = load([runname '_stats.mat'],'dtime');
-        dtime = dtime_orig.dtime(end-size(ihm_half,3):end);
-        clear ihm_half
-        clear dtime_orig
-
-    elseif strcmp(runname_title ,[ 'miscut\_32\_40'])
-        load([runname '_ihm_half.mat']);
-        ihm = ihm_half;
-        dtime_orig = load([runname '_stats.mat'],'dtime');
-        dtime = dtime_orig.dtime(end-size(ihm_half,3):end);
-        clear ihm_half
-        clear dtime_orig
-
+    for ss = 1:size(runnames,1)
+        
+        path = paths{ss,:};
+        runname = [paths{ss,:} runnames{ss,:}];
+        runname_title = runname_titles{ss,:};
+                
+        if strcmp(runname_title ,[ 'miscut\_32\_29'])
+            load([runname '_ihm.mat']);
+            load([runname '_stats.mat'],'dtime');
+        elseif strcmp(runname_title ,[ 'miscut\_32\_30'])
+            load([runname '_ihm.mat']);
+            load([runname '_stats.mat'],'dtime');
+        elseif strcmp(runname_title ,[ 'miscut\_32\_35'])
+            load([runname '_ihm.mat']);
+            load([runname '_stats.mat'],'dtime');
+        elseif strcmp(runname_title ,[ 'miscut\_32\_36'])
+            load([runname '_ihm.mat']);
+            load([runname '_stats.mat'],'dtime');
+            
+        elseif strcmp(runname_title ,[ 'miscut\_32\_37'])
+            load([runname '_ihm_half.mat']);
+            ihm = ihm_half;
+            dtime_orig = load([runname '_stats.mat'],'dtime');
+            dtime = dtime_orig.dtime(end-round(size(ihm_half,3)/2):end);
+            clear ihm_half
+            clear dtime_orig
+        elseif strcmp(runname_title ,[ 'miscut\_32\_38'])
+            load([runname '_ihm_half.mat']);
+            ihm = ihm_half;
+            dtime_orig = load([runname '_stats.mat'],'dtime');
+            dtime = dtime_orig.dtime(end-size(ihm_half,3):end);
+            clear ihm_half
+            clear dtime_orig
+            
+        elseif strcmp(runname_title ,[ 'miscut\_32\_39'])
+            load([runname '_ihm_half.mat']);
+            ihm = ihm_half;
+            dtime_orig = load([runname '_stats.mat'],'dtime');
+            dtime = dtime_orig.dtime(end-size(ihm_half,3):end);
+            clear ihm_half
+            clear dtime_orig
+            
+        elseif strcmp(runname_title ,[ 'miscut\_32\_40'])
+            load([runname '_ihm_half.mat']);
+            ihm = ihm_half;
+            dtime_orig = load([runname '_stats.mat'],'dtime');
+            dtime = dtime_orig.dtime(end-size(ihm_half,3):end);
+            clear ihm_half
+            clear dtime_orig
+            
+        end
+        
+        if strcmp(runname_title ,[ 'miscut\_32\_37'])
+            dt_minML = dtime(end-round(2.50e4/2));%dtime(30);%dtime(end-round(2.5e4/8));%dtime(end-6.3e3);% % dtime(end-1e4);
+            
+        else
+            dt_minML = dtime(end-2.50e4);%dtime(30);%dtime(end-round(2.5e4/8));%dtime(end-6.3e3);% % dtime(end-1e4);
+        end
+        dt_maxML = dtime(end);%dtime(250);%dtime(end-1.25e4);%
+        %idt = dtime >dt_minML;
+        idt = (dtime>dt_minML & dtime<dt_maxML);
+        
+        idt_half =round(size(idt,1)/2);
+        
+        ihm_struct(ss).ihm = squeeze(ihm(:,:,idt_half));
+        
+        
+        
+        
     end
     
-    if strcmp(runname_title ,[ 'miscut\_32\_37'])
-         dt_minML = dtime(end-round(2.50e4/2));%dtime(30);%dtime(end-round(2.5e4/8));%dtime(end-6.3e3);% % dtime(end-1e4);
-
-    else
-        dt_minML = dtime(end-2.50e4);%dtime(30);%dtime(end-round(2.5e4/8));%dtime(end-6.3e3);% % dtime(end-1e4);
-    end
-    dt_maxML = dtime(end);%dtime(250);%dtime(end-1.25e4);%
-    %idt = dtime >dt_minML;
-    idt = (dtime>dt_minML & dtime<dt_maxML);
-    
-    idt_half =round(size(idt,1)/2);
-    
-    ihm_struct(ss).ihm = squeeze(ihm(:,:,idt_half));
-    
-
-    
-    
+    save('./allmiscut_mat_files/ihm_struct_file.mat','ihm_struct')
+else
+    load('./allmiscut_mat_files/ihm_struct_file.mat','ihm_struct');
 end
 
 nrow_orig = size(ihm_struct(1).ihm,1);
@@ -667,6 +684,7 @@ ncol_orig = size(ihm_struct(1).ihm,2);
 
 
 %% Plot ihm
+
 
 struct_ranges_ihm = struct_ranges;
 struct_ranges_ihm.QX = [1:ncol];
@@ -680,7 +698,7 @@ figNum = 13;
 %time_frame = 130;
 clear title_figure;
 for ss = 1:size(runname_titles,1)
-    title_figure(ss,:) = {[runname_titles{ss,:} ' ihm']};
+    title_figure(ss,:) = {[runname_titles{ss,:}];[' ihm; g.r = ' num2str(growth_rates_all(ss))]};
     ihm_struct(ss).ihm_square = ihm_struct(ss).ihm(struct_ranges_ihm.row_range,struct_ranges_ihm.col_range,:);
 end
 
@@ -689,3 +707,99 @@ Function_display_sos.make_figure_2D_realspace( title_figure,ihm_struct,'ihm_squa
     'CLIM',[0 35],...
     'POSITION',POSITION,'PAPERPOSITION',PAPERPOSITION,...
     'plot_averages',0);
+
+
+%% Load and plot the diffraction patterns
+
+ clear time_frame;
+computer_script;
+ 
+ if III_flag == 0
+     for ss = 1:size(runnames,1)
+         path = paths{ss,:};
+         runname = [paths{ss,:} runnames{ss,:}];
+         runname_title = runname_titles{ss,:};
+         
+         load([runname '_corr_dt_L0p5.mat'],'III_resize');
+         
+         time_frame_index = round(size(III_resize,3)/2);
+         
+         III_struct(ss).III = III_resize(:,:,time_frame_index);
+     end
+     save('./allmiscut_mat_files/III_struct_file.mat','III_struct')
+     
+     
+ else
+     load('./allmiscut_mat_files/III_struct_file.mat','III_struct')
+     
+ end
+ 
+ 
+ 
+ % Figure 10: diffraction pattern
+figNum = 14;
+% time frame to represent:
+%time_frame = [1e4 1e4 5e3 1e3];
+clear title_figure;
+for ss = 1:size(runname_titles,1)
+    iii = 1;
+    jjj = 1;
+    time_frame(ss) = time_frame_index;
+    title_figure(ss,:) = {[runname_titles{ss,:}];[ ' III, @ ' num2str(time_frame(ss),'%.1e') ' g.r = ' num2str(growth_rates_all(ss))]};%[runname_titles(ss,:) ' Diffracted intensity, time frame = ' num2str(time_frame)];
+    %map2D(ss).III = damHW_struct(ss).III(:,:,time_frame(ss));
+end
+
+
+Function_display_sos.make_figure_2D( title_figure,III_struct,'III',struct_ranges,iCTR,figNum,...
+    'CLIM',[-10 -2],...
+    'POSITION',POSITION,'PAPERPOSITION',PAPERPOSITION,...
+    'plot_averages',0);
+
+
+
+ 
+ %% Load and plot the Ibar
+ clear time_frame;
+commputer_script;
+ 
+ if Ibar_flag == 0
+     for ss = 1:size(runnames,1)
+         path = paths{ss,:};
+         runname = [paths{ss,:} runnames{ss,:}];
+         runname_title = runname_titles{ss,:};
+         
+         load([runname '_corr_dt_L0p5.mat'],'Ibar');
+         
+         time_frame_index = round(size(Ibar,3)/2)
+         
+         Ibar_struct(ss).Ibar = Ibar(:,:,time_frame_index);
+     end
+     save('./allmiscut_mat_files/Ibar_struct_file.mat','Ibar_struct')
+     
+     
+ else
+     load('./allmiscut_mat_files/Ibar_struct_file.mat','Ibar_struct')
+     
+ end
+ 
+
+ 
+ % Figure 10: diffraction pattern
+figNum = 15;
+% time frame to represent:
+%time_frame = [1e4 1e4 5e3 1e3];
+clear title_figure;
+for ss = 1:size(runname_titles,1)
+    iii = 1;
+    jjj = 1;
+    time_frame(ss) = time_frame_index;
+    title_figure(ss,:) = {[runname_titles{ss,:} ' Ibar, @ ' num2str(time_frame(ss),'%.1e')]};%[runname_titles(ss,:) ' Diffracted intensity, time frame = ' num2str(time_frame)];
+    %map2D(ss).III = damHW_struct(ss).III(:,:,time_frame(ss));
+end
+
+
+Function_display_sos.make_figure_2D( title_figure,Ibar_struct,'Ibar',struct_ranges,iCTR,figNum,...
+    'CLIM',[-10 -2],...
+    'POSITION',POSITION,'PAPERPOSITION',PAPERPOSITION,...
+    'plot_averages',0);
+
